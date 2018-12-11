847452130f81261e429648fa670ec3776725c138
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2018-12-11 16:22:14 -0500

    Now returning number of days in age

--- Person.php
+++ Person.php
@@ -16,8 +16,13 @@ class Person {
         }
         $this->age = $age; 
     }
+
+    public function getAge()
+    {
+        return $this->age * 365;
+    }
 }
 
 $john = new Person('John Doe');
-$john->setAge(17);
-var_dump($john);
+$john->setAge(30);
+var_dump($john->getAge());
