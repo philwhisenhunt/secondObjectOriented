a38e8205f4edd8e55f4124c3cf415ac3996e65d9
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2018-12-17 15:17:37 -0500

    Added to staff

--- Messages.php
+++ Messages.php
@@ -39,8 +39,10 @@ class Staff {
 }
 
 $jeffrey = new Person('Jeffrey Way');
-$staff = new Staff;
+$staff = new Staff([$jeffrey]);
 $laracasts = new Business($staff);
 
-$laracasts->hire($jeffrey);
+// $laracasts->hire($jeffrey);
+$laracasts->hire(new Person('Jane Doe'));
+
 var_dump($staff);
