2a694dc939e0ff6bec562d34f155f45bacc165bd
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2018-12-16 23:20:35 -0500

    assigned staff to protected property

--- Messages.php
+++ Messages.php
@@ -11,9 +11,15 @@ class Person {
 }
 
 class Business {
+    protected $staff;
+    public function __construct(Staff $staff)
+    {
+        $this->staff = $staff;
+    }
+
     public function hire(Person $person)
     {
-
+        //add $person to the staff collection 
     }
 }
 
