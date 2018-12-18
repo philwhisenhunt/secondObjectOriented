77b6c0a022c6cb368a2c6bb7071c8e102d656a76
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2018-12-18 12:47:48 -0500

    Added pause I found, now time to edit it

--- zipper.php
+++ zipper.php
@@ -22,6 +22,17 @@ function zipper($array1, $array2){
         }
         
     }
+
+    echo "Are you sure you want to do this?  Type 'yes' to continue: ";
+    $handle = fopen ("php://stdin","r");
+    $line = fgets($handle);
+    if(trim($line) != 'yes'){
+        echo "ABORTING!\n";
+        exit;
+    }
+    fclose($handle);
+    echo "\n"; 
+    echo "Thank you, continuing...\n";
     return $mergedArray;
 
 
