a428689e49e106af39b6073694ea8faf9fb86248
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2018-12-18 13:44:52 -0500

    refer to this later to see what wasn't working

--- zipper.php
+++ zipper.php
@@ -1,6 +1,31 @@
 <?php 
 
 function zipper($array1, $array2){
+    $array1 = [];
+    echo "Type in a value for array1";
+    $handle = fopen ("php://stdin","r");
+    $array1[0] = array(fgets($handle));
+
+    echo "Type in a value for array2";
+    $handle = fopen ("php://stdin","r");
+    $array1[1] = array(fgets($handle));
+
+    echo "Type in value for array3";
+    $handle = fopen ("php://stdin","r");
+    $array1[2] = array(fgets($handle));
+
+
+
+    // if(($array1) != Array()){
+    //     echo "ABORTING!\n";
+    //     exit;
+    // }
+    fclose($handle);
+    echo "\n"; 
+    echo "Thank you for your input, here is your zippered array...\n";
+    print_r($array1);
+
+    //resume original code
   
     $mergedArray = Array();
 
@@ -22,28 +47,13 @@ function zipper($array1, $array2){
         }
         
     }
-
-    echo "Are you sure you want to do this?  Type 'yes' to continue: ";
-    $handle = fopen ("php://stdin","r");
-    $line = fgets($handle);
-    if(trim($line) != 'yes'){
-        echo "ABORTING!\n";
-        exit;
-    }
-    fclose($handle);
-    echo "\n"; 
-    echo "Thank you, continuing...\n";
+    return $array1;
     return $mergedArray;
-
-
-
-
-
 
 
 }
 
-$array1 = Array(1,1,1,1,1,1,1,7);
+// $array1 = Array(1,1,1,1,1,1,1,7);
 $array2 = Array(0,0,0,0,0,0,0,0,0,0,0,0,0);
 
 print_r(zipper($array1, $array2));
