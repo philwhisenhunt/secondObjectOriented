b66f9e4cf7fc684a30459ea36f3560a82d866efb
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2018-12-18 15:53:08 -0500

    checking in final version of zipper

--- zipper.php
+++ zipper.php
@@ -1,32 +1,7 @@
 <?php 
 
-function zipper( $array2){
-    $array1 = [];
-    echo "Type in a value for array1";
-    $handle = fopen ("php://stdin","r");
-    $array1[0] = array(fgets($handle));
+function zipper($array1, $array2){
 
-    echo "Type in a value for array2";
-    $handle = fopen ("php://stdin","r");
-    $array1[1] = array(fgets($handle));
-
-    echo "Type in value for array3";
-    $handle = fopen ("php://stdin","r");
-    $array1[2] = array(fgets($handle));
-
-
-
-    // if(($array1) != Array()){
-    //     echo "ABORTING!\n";
-    //     exit;
-    // }
-    fclose($handle);
-    echo "\n"; 
-    echo "Thank you for your input, here is your zippered array...\n";
-    print_r($array1);
-
-    //resume original code
-  
     $mergedArray = Array();
 
     $maxSize = count($array1);
@@ -47,15 +22,26 @@ function zipper( $array2){
         }
         
     }
-    return $array1;
+
     return $mergedArray;
-
-
-
 }
 
-// $array1 = Array(1,1,1,1,1,1,1,7);
+$array1 = Array(1,1,1,1,1,1,1,7);
 $array2 = Array(0,0,0,0,0,0,0,0,0,0,0,0,0);
 
-// print_r(zipper($array1, $array2));
-print_r(zipper($array2));
+print_r(zipper($array1, $array2));
+
+//
+// How to use this? When I tried before had trouble with the way you take in input. 
+
+
+// echo "Are you sure you want to do this?  Type 'yes' to continue: ";
+// $handle = fopen ("php://stdin","r");
+// $line = fgets($handle);
+// if(trim($line) != 'yes'){
+//     echo "ABORTING!\n";
+//     exit;
+// }
+// fclose($handle);
+// echo "\n"; 
+// echo "Thank you, continuing...\n";
